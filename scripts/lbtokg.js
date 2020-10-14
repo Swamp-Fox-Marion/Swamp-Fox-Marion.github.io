5e41f339d74405c2a7c0bbbd83ec15b9a01d16fa
Swamp-Fox-Marion <all4moyers@gmail.com>
2020-10-13 20:01:51 -0600

    complete

--- scripts/lbtokg.js
+++ scripts/lbtokg.js
@@ -6,15 +6,13 @@ function multiplyNumbers() {
     
     //processing
     number1 = parseFloat(number1);
-    
-    //add the two numbers together and store in my output variable
     //processing needs to convert number1 from pounds to kilograms
     //1 pounds is = to .456592 kilograms
     let kilograms = .456592 * number1;
    
     //output
-    //print to screen the conversion
-    document.getElementById("kilograms").innerHTML = "The conversion <strong>is</strong> " + kilograms.toFixed(1) + ".";
+    //print to console the conversion
+    document.getElementById("kilograms").innerHTML = "The answer you are looking for <strong>is</strong> " + kilograms.toFixed(1) + " kilograms.";
 
 
 }
