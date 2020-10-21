f61a74ac2c471daaedab3570d36f617fc87db139
Swamp-Fox-Marion <all4moyers@gmail.com>
2020-10-21 14:02:34 -0600

    comment

--- scripts/footwear.js
+++ scripts/footwear.js
@@ -13,5 +13,7 @@ function decideForMe(){
         default:
             document.getElementById("shoes").innerText ="Who cares about the weather just wear shoes you love today";
             break;
+            //using switch statement was fewest lines of code
+            //simple for me to know what results I wanted and the results I got
     }    
 }
