22e29f9083514749c5616871ae2f598d4beb0004
Swamp-Fox-Marion <all4moyers@gmail.com>
2020-10-21 13:43:07 -0600

    style

---
+++ scripts/calc.js
@@ -0,0 +1,15 @@
+//input is user input change string to integer
+let a = parseInt(
+let b = parseInt(  
+
+//processing is determining the options
+switch (variable) {
+    case vaue:
+        statement;
+        break;
+    default:
+        statement;
+        break;
+
+}
+//output is display the correct statement for user
