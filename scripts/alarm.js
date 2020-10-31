d185751f418a1329e6848b866e41a9c1a83db482
Swamp-Fox-Marion <all4moyers@gmail.com>
2020-10-30 19:05:25 -0600

    no function

--- scripts/alarm.js
+++ scripts/alarm.js
@@ -1,4 +1,4 @@
-function alarm() {
+
     /* On all weekdays (Monday through Friday) that are not holidays,
         your program should output “Get up!” On all other days
        (weekends and holidays) your program should output “Sleep in.”
@@ -9,29 +9,29 @@ function alarm() {
         occur on a fixed day each year.*/
 
         // I put this here hoping it would display the date on the console
-    document.getElementById("date").innerHTML = new Date();
+document.getElementById("date").innerHTML = new Date();
        
         
-    let now = new Date();   
-    let month = now.getMonth();
-    let dayOfMonth = now.getUTCDate();
-    let dayOfWeek = now.getUTCDay();
+let now = new Date();   
+let month = now.getMonth();
+let dayOfMonth = now.getUTCDate();
+let dayOfWeek = now.getUTCDay();
 // I used == because I was getting error with only =
-    if (month == 0 && dayOfMonth == 1) {
-        message = "Sleep in.";
-    }
-    else if (month== 6 && dayOfMonth == 4) {
-        message = "Sleep in.";
-    }
-    else if (month == 11 && dayOfMonth == 25) {
-        message = "Sleep in.";
-    }
-    else if (dayOfWeek == 6 || dayOfWeek == 0) {
-        message = "Sleep in.";
-    }
-    else {
-        message = "Get up!";
-    }
-    document.getElementById("message").innerHTML = message;
-    
+if (month == 0 && dayOfMonth == 1) {
+    message = "Sleep in.";
 }
+else if (month== 6 && dayOfMonth == 4) {
+    message = "Sleep in.";
+}
+else if (month == 11 && dayOfMonth == 25) {
+    message = "Sleep in.";
+}
+else if (dayOfWeek == 6 || dayOfWeek == 0) {
+    message = "Sleep in.";
+}
+else {
+    message = "Get up!";
+}
+document.getElementById("message").innerHTML = message;
+    
+
