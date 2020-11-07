a65d22c657c2d313c8986d118d5dc46bf9f6d6eb
Swamp-Fox-Marion <all4moyers@gmail.com>
2020-11-07 12:41:25 -0700

    css

--- scripts/sumOdds.js
+++ scripts/sumOdds.js
@@ -5,26 +5,31 @@ For example,
 if a user entered 10, your program would add 1 + 3 + 5 + 7 + 9 and output 25.
 
 If a user entered 11, your program would add 1 + 3 + 5 + 7 + 9 + 11 and output 36.*/
-//to count by odds use (i % 2 !== 0)
+
 
 //user input will be the ending for the repetition
 
 //for (let i = 1; i<= userInput; i++)
-    //if (i % 2 !== 0) 
+   
 function sumOdds() {
     let n = document.getElementById("n").value;
+    let count = 0;
+    let i;
+
+
     
     /*Processing is placing the input into the fuction and repeat until perameters 
 have been met*/
-    for (let i = 1; i <= n; i+=2){
+    for (let i=1; i <= n; i++){
+        count += i;
+        i++;
         
-        message = `${i}`;
         
     
     }
     /*Output is the repetition has met its concluding factors and the results displays
 to the user*/
-    document.getElementById("output").innerText = "Addition with odd numbers, the sum is " + message
+    document.getElementById("output").innerText = "The sum of the odds is " + count
 }
    
 
