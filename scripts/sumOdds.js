4e88f76748b616e4785c37be53797a4b60618bc1
Swamp-Fox-Marion <all4moyers@gmail.com>
2020-11-06 17:18:11 -0700

    fixloops

---
+++ scripts/sumOdds.js
@@ -0,0 +1,31 @@
+/*In a file named, sumOdds.html, write a program that asks the user for an
+ integer n and then outputs the sum of all the odd integers between 1 and n, 
+ inclusive.
+For example,
+if a user entered 10, your program would add 1 + 3 + 5 + 7 + 9 and output 25.
+
+If a user entered 11, your program would add 1 + 3 + 5 + 7 + 9 + 11 and output 36.*/
+//to count by odds use (i % 2 !== 0)
+
+//user input will be the ending for the repetition
+
+//for (let i = 1; i<= userInput; i++)
+    //if (i % 2 !== 0) 
+function sumOdds() {
+    let n = document.getElementById("n").value;
+    
+    /*Processing is placing the input into the fuction and repeat until perameters 
+have been met*/
+    for (let i = 1; i <= n; i+=2){
+        
+        message = `${i}`;
+        
+    
+    }
+    /*Output is the repetition has met its concluding factors and the results displays
+to the user*/
+    document.getElementById("output").innerText = "Addition with odd numbers, the sum is " + message
+}
+   
+
+
