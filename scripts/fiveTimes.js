f8cc3f47f55182c35ce020fb3fdc9a1b4fe1a432
Swamp-Fox-Marion <all4moyers@gmail.com>
2020-11-14 19:51:23 -0700

    move

--- scripts/fiveTimes.js
+++ scripts/fiveTimes.js
@@ -1,8 +1,10 @@
+//input define variables
 let i = 1;
 let answer = "";
+//processing a repeating loop
 while (i <= 12) {
     answer +=`5 x ${i} = ${5 * i} <br>`;
     i++;
 }
-
+//output to the console the repeating message
 document.getElementById("output").innerHTML = answer
